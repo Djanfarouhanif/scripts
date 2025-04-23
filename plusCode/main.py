a3fea723f83afc63950f4835c89fc7eae7d56612
unknown <hanifdjanfarou8@gmail.com>
2025-04-23 21:12:44 +0000

    j'ai ajouter la fonction pour decoder plus code

--- plusCode/main.py
+++ plusCode/main.py
@@ -3,7 +3,21 @@ from openlocationcode import openlocationcode
 latitude = 6.1319
 longitude = 1.2228
 
-plus_code = openlocationcode.encode(latitude,longitude)
+def genaretePlusCode(latitude, longitude):
+
+    plus_code = openlocationcode.encode(latitude,longitude)
+
+    return plus_code
 
 
-print("Plus code : ", plus_code)
+def decodePlusCode(plusCode):
+    decoded = openlocationcode.decode(plusCode)
+    latitude = decoded.latitudeCenter
+    longitude = decoded.longitudeCenter
+
+    return latitude, longitude
+plusCode = genaretePlusCode(latitude,longitude)
+
+latitude, longitude = decodePlusCode(plusCode)
+
+print(plusCode, latitude, longitude)
